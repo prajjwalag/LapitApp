3715db326e795a5d27205079b9d22fa7d201c2d6
Prajjwal Agrahari <prajjwal.agrahari16@gmail.com>
2021-02-28 18:16:16 +0530

    In Video 4, after adding intent to register activity

--- app/src/main/java/com/prajjwal/lapitapp/WelcomeActivity.java
+++ app/src/main/java/com/prajjwal/lapitapp/WelcomeActivity.java
@@ -2,13 +2,29 @@ package com.prajjwal.lapitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Button mRegBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        mRegBtn = (Button) findViewById(R.id.welcome_reg_btn);
+
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg_intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
+                startActivity(reg_intent);
+
+            }
+        });
     }
 }
