51499d7726e1bba80f0d92b0d4b31d9012fbf9aa
Prajjwal Agrahari <prajjwal.agrahari16@gmail.com>
2021-02-28 18:07:12 +0530

    After Video 3

--- app/src/main/java/com/prajjwal/lapitapp/MainActivity.java
+++ app/src/main/java/com/prajjwal/lapitapp/MainActivity.java
@@ -2,13 +2,33 @@ package com.prajjwal.lapitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser==null) {
+            Intent welcomeIntent = new Intent(MainActivity.this, WelcomeActivity.class);
+            startActivity(welcomeIntent);
+            finish();
+        }
     }
 }
