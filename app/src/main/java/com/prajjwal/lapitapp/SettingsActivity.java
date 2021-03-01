62c8b528e470903051a2af2beaef0e4f6f0de5e6
Prajjwal Agrahari <prajjwal.agrahari16@gmail.com>
2021-03-01 13:50:09 +0530

    After Video 10, retrieved values from database and displayed it in account settings page

--- app/src/main/java/com/prajjwal/lapitapp/SettingsActivity.java
+++ app/src/main/java/com/prajjwal/lapitapp/SettingsActivity.java
@@ -1,14 +1,63 @@
 package com.prajjwal.lapitapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private DatabaseReference mUserDatabase;
+    private FirebaseUser mCurrentUser;
+
+    private CircleImageView mDisplayImage;
+    private TextView mName;
+    private TextView mStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mDisplayImage = (CircleImageView) findViewById(R.id.settings_image);
+        mName = (TextView) findViewById(R.id.settings_display_name);
+        mStatus = (TextView) findViewById(R.id.settings_about);
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        String current_uid = mCurrentUser.getUid();
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child("name").getValue().toString();
+                String image = snapshot.child("image").getValue().toString();
+                String status = snapshot.child("status").getValue().toString();
+                String thumb_image = snapshot.child("thumb_image").getValue().toString();
+
+                mName.setText(name);
+                mStatus.setText(status);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
