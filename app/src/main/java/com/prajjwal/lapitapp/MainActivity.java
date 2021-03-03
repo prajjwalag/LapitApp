9c7fb2bc8fa4447128777cb7e7624b90f91edf93
Prajjwal Agrahari <prajjwal.agrahari16@gmail.com>
2021-03-03 13:21:22 +0530

    After video 14, completed all users

--- app/src/main/java/com/prajjwal/lapitapp/MainActivity.java
+++ app/src/main/java/com/prajjwal/lapitapp/MainActivity.java
@@ -83,6 +83,10 @@ public class MainActivity extends AppCompatActivity {
             Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(settingsIntent);
         }
+        if(item.getItemId() == R.id.main_all_btn) {
+            Intent AllUsersIntent = new Intent(MainActivity.this, UsersActivity.class);
+            startActivity(AllUsersIntent);
+        }
 
         return true;
     }
--- app/src/main/java/com/prajjwal/lapitapp/UsersActivity.java
+++ app/src/main/java/com/prajjwal/lapitapp/UsersActivity.java
@@ -1,14 +1,93 @@
 package com.prajjwal.lapitapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UsersActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView mUsersList;
+
+    private DatabaseReference mUsersDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+
+        mToolbar = (Toolbar) findViewById(R.id.users_app_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("All Users");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mUsersList = (RecyclerView) findViewById(R.id.users_list);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Users> options = new FirebaseRecyclerOptions.Builder<Users>().
+                setQuery(mUsersDatabase, Users.class).build();
+
+        FirebaseRecyclerAdapter<Users, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull Users model) {
+                holder.userName.setText(model.getName());
+                holder.userStatus.setText(model.getStatus());
+                if(model.getImage().equals("default")) {
+                    Picasso.get().load(R.drawable.avatar).into(holder.userImage);
+                } else {
+                    Picasso.get().load(model.getImage()).into(holder.userImage);
+                }
+            }
+
+            @NonNull
+            @Override
+            public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_single_layout, parent, false);
+                return new UsersViewHolder(view);
+            }
+        };
+        firebaseRecyclerAdapter.startListening();
+        mUsersList.setAdapter(firebaseRecyclerAdapter);
+
+
+    }
+
+    public class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        TextView userName, userStatus;
+        CircleImageView userImage;
+
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userName = itemView.findViewById(R.id.user_single_name);
+            userStatus = itemView.findViewById(R.id.user_single_status);
+            userImage = itemView.findViewById(R.id.user_single_image);
+        }
     }
 }
